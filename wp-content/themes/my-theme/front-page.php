2cb8ef3ccaffd52dd8294990292259106115afc5
yimie <bunyimkong@gmail.com>
2026-01-09 00:39:15 +0700

    feat: add navbar, bunyim

--- wp-content/themes/my-theme/front-page.php
+++ wp-content/themes/my-theme/front-page.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 
+<?php get_header();?>
---
+++ wp-content/themes/my-theme/header.php
@@ -0,0 +1,72 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/components/header.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+
+<header class="navbar">
+
+    <div class="header-inner container">
+
+        <!-- Logo -->
+        <div class="logo-block">
+            <a href=""><img src="<?php echo get_template_directory_uri() . '/asset/images/logo.png'; ?>" width="160"></a>
+            
+        </div>
+
+        <!-- Top Bar -->
+        <div class="top-bar">
+
+            <div class="top-info">
+                <span>info@almanahospital.com.sa</span>
+            </div>
+
+            <div class="top-info">
+                <span>P.O. Box 3568, Al Khobar 31952, Saudi Arabia</span>
+            </div>
+
+            <div class="top-info">
+                <img src="images/call.png" alt="">
+                <span>+966 920033440</span>
+            </div>
+
+            <!-- Social link -->
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-x-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- ===== MENU AREA ===== -->
+    <div class="menu-header container">
+
+        <nav class="nav-menu">
+            <a href="#">Home</a>
+            <a href="#">About us</a>
+            <a href="#">Doctors</a>
+
+            <div class="dropdown">
+                <a href="#">Pages <i class="fas fa-chevron-down"></i></a>
+                <div class="dropdown-menu">
+                    <a href="#">Page 1</a>
+                    <a href="#">Page 2</a>
+                </div>
+            </div>
+
+            <a href="#">Blog</a>
+        </nav>
+
+        <div class="header-button">
+            <a href="#" class="appointment-btn">
+                Make An Appointment <i class="fas fa-chevron-right"></i>
+            </a>
+        </div>
+
+    </div>
+
+</header>
