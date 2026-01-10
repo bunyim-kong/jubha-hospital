c1c14f8efb4fc8ff6b01cf05c11dc46dec78848f
yimie <bunyimkong@gmail.com>
2026-01-11 01:12:28 +0700

    feat: hospital & clinic page, add footer, by bunyim

---
+++ wp-content/themes/my-theme/footer.php
@@ -0,0 +1,78 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css"/>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/components/footer.css"/>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
+
+<footer>
+  <div class="footer-container">
+    <div class="footer-logo">
+        <a href="">
+        <img src="<?php echo get_template_directory_uri() . '/asset/images/logo.png'; ?>" alt="Hospital Logo">
+        </a>
+
+        <p>
+            Since our inception in 1949, we have a legacy of excellence. We are a family hospital fully dedicated to caring for the needs of the patients. 
+        </p>
+    </div>
+
+    <!-- Contact / Find Us -->
+    <div class="footer-section">
+
+      <h2>FIND US</h2>
+
+      <p>P.O. Box: 3568, Al Khobar 31952,
+        Kingdom of Saudi Arabia</p>
+      <ul class="contact-list"> 
+
+        <li>
+          <a href="">
+            <i class="fas fa-phone"></i> +966 920033440
+          </a>
+        </li>
+        
+        <li>
+          <a href="">
+            <i class="fas fa-envelope"></i>  info@almanahospital.com.sa 
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- About Pages -->
+    <div class="footer-section">
+        <h2>ABOUT Hospital</h2>
+
+        <ul class="page-list">
+            <li><a href="">Find Doctor</a></li>
+            <li><a href="">News & Events</a></li>
+            <li><a href="">Get In Touch</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-section">
+        <!-- Connect with us -->
+        <h3>CONNECT WITH US</h3>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/intercarehospital/" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.linkedin.com/company/intercarehospital" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://t.me/intercarehospital" target="_blank"><i class="fab fa-telegram"></i></a>
+            <a href="https://www.youtube.com/@intercarehospital" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+     
+        <br>
+      <!-- Newsletter -->
+        <div class="footer-section">
+            <h3>SUBSCRIBE OUR NEWSLETTER</h3>
+            <form class="newsletter">
+                <input type="email" placeholder="Enter Your Email..." required>
+                <button type="submit">Subscribe</button>
+            </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        &copy; 2026 Intercare Hospital. All rights reserved
+    </div>
+</footer>
--- wp-content/themes/my-theme/page-hospital-and-clinic.php
+++ wp-content/themes/my-theme/page-hospital-and-clinic.php
@@ -15,4 +15,97 @@
     <p class="head_des">
         Keeping with our legacy of innovation and expansion, we have outspread into four tertiary care hospitals, 8 outpatient medical centers across the Eastern Province.
     </p>
+
+    <div class="location-content-wrapper">
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-medical-center-rakah1/DSC06581.jpg" alt="Hospital 1"/>
+
+            <div class="location-info">
+                <h1>AMC Rakkah</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-hospital-khobar1/DJI_0093_Edit_2_1.png" alt="Hospital 2"/>
+
+            <div class="location-info">
+                <h1>AGH Khobar</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-hospital-dammam/00.jpg" alt="Hospital 3"/>
+
+            <div class="location-info">
+                <h1>AGH Dammam</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-oncology-center-dammam1/DJI_0019_3_JACAL.jpg" alt="Hospital 4"/>
+
+            <div class="location-info">
+                <h1>Almana Oncology Center – Dammam</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-hospital-al-aziziah-dammam/_DSC0512.png" alt="Hospital 5"/>
+
+            <div class="location-info">
+                <h1>AGH Aziziah Dammam</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-hospital-jubail/jubail_building_rehan.jpg" alt="Hospital 6"/>
+
+            <div class="location-info">
+                <h1>AGH Jubail</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-medical-center-jubail/AMC_jubail.jpg" alt="Hospital 7"/>
+
+            <div class="location-info">
+                <h1>AGH Jubail</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+
+        <article class="location-card">
+            <img src="https://ds4kyztv1rtw.cloudfront.net/uploads/almana-hospital-al-ahsa1/DSC02925_copy.jpg" alt="Hospital 8"/>
+
+            <div class="location-info">
+                <h1>AGH Hofuf</h1>
+
+                <a href=""><i class="fas fa-map-marker-alt"></i> View Detail</a>
+                <a href=""><i class="fas fa-calendar-check"></i> Timings</a>
+            </div>
+        </article>
+    </div>
 </section>
+
+<?php get_footer();?>
