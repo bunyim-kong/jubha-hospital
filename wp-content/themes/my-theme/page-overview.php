ce8728c21529b27efc49b144d7d7593226e5e6a5
yimie <bunyimkong@gmail.com>
2026-01-13 16:51:12 +0700

    feat: about page 10% by bunyim

---
+++ wp-content/themes/my-theme/page-overview.php
@@ -0,0 +1,74 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css"/>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/pages/about.css"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100&icon_names=favorite,home,search,settings" rel="stylesheet" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=play_circle" />
+
+<?php get_header();?>
+
+<div class="header-page">
+    <span><a href="<?php echo home_url('home'); ?>">Home</a> / About us / Overview</span>
+    <h1 class="">Overview</h1>
+</div>
+
+<section class="about-section container">
+    <div class="about-content content-wrapper">
+        <div class="overview-txt">
+            <h1 class="title_head">The Hospital</h1>
+            <span class="highligh_title">Seven decades of care and quality</span>
+
+            <p class="text_des">Since our inception in 1949, we have a legacy of excellence. We are a family hospital fully dedicated to caring for the needs of the patients, communities, and Kingdom we serve to keep healthy. Almana believes in offering compassionate care, inspired by family values which has kept patients happier and healthier for over 75 years – this is a deeply ingrained part of our DNA. What’s more, our rich legacy of delivering person-centric, specialized, integrated, and comprehensive care, makes Almana a trusted institution and drives our journey to meet the growing needs of today and generations to come.</p>
+        </div>
+        
+        <div class="overview-vdo">
+            <a href="https://www.youtube.com/watch?v=QdM77xj2C0I"><img src="https://ds4kyztv1rtw.cloudfront.net/uploads/overview/s_JAAN8284.jpg" alt=""></a>
+
+            <div class="vdo-play-icon">
+                <span class="material-symbols-outlined">play_circle</span>
+            </div>
+
+            <div class="text-overlay">
+                <span>Serving the <br>
+                community since</span>
+                <h1>1949</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="overview-infor">
+            <p class="text_des">
+                As the first private general hospital in the Eastern Province, as well as one of the oldest and largest in the Gulf region, Almana Hospitals continues its tradition of excellence and remains the preferred tertiary care hospital among the public and referral health centers and care providers in the region.
+            </p>
+
+            <p class="text_des">
+                Almana’s network of leading medical facilities and highly skilled clinical staff pair decades of distinguished experience with smart medical advancements and technology to deliver exceptional healthcare covering prevention, diagnosis, treatment, and long-term care. Renowned for always being able to find a solution to your needs, Almana has a strong track record of delivering life-changing specialized, integrated, and comprehensive care to patients across all age groups and healthcare needs no matter what stage of your journey – whether it’s prevention, diagnosis, treatment, or cure.
+            </p>
+
+            <p class="text_des">
+                Providing exceptional healthcare and understanding our patients and partners is in our DNA. Over the years, we have developed our healthcare offering in line with the families and communities we have cared for, constantly adapting to their needs.
+            </p>
+
+            <p class="text_des">
+                Aligned with the Saudi Vision 2030, we are constantly enhancing our services, providing specialized, integrated, and comprehensive care to improve health and wellbeing, and thus enabling a healthy and vibrant society in the Kingdom.
+            </p>
+
+            <p class="text_des">
+                We are also investing heavily in technology within our hospitals for process automation and unifying patient care records to improve health information exchange and provide seamless service for patients. A central command center has been established to improve patient outcomes by coordinating care between our hospital locations.
+            </p>
+
+            <p class="text_des">
+                We continuously foster a culture of innovation to create value in areas of high unmet medical needs across the Kingdom, such as our foot disease and diabetes center, as well as the dedicated oncology center in Dammam are the first and only ones in the Eastern Province. We also launched the first 24-hour dedicated stroke center in the region.
+            </p>
+
+            <p class="text_des">
+                We share the ambition of Saudi Vision 2030 to increase the participation of Saudi female talents in the workforce. Almana is taking several steps to train and recruit local medical professionals. Through our official healthcare training academy, Mohammed Almana College for Medical Science, we are helping develop the next generation of healthcare professionals, graduating over 180 Saudi nurses each year. We have females leading our medical departments and we are looking to increase this even further by 20 percent over the next five years.
+            </p>
+
+            <p class="text_des">
+                At Almana, we believe in offering compassionate care inspired by family values which has kept patients happier and healthier for over 75 years. It is our rich legacy of delivering person-centric, specialized, integrated, and comprehensive care that makes us a trusted institution and drives our journey to meet the growing needs of today and generations to come.
+            </p>
+        </div>
+    </div>
+</section>
